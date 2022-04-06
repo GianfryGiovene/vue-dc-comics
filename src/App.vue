180cf18ff573e49c5111297148700fe2ee02301e
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-04-06 14:20:39 +0200

    Header added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    
+    <HeaderSide />
   </div>
 </template>
 
 <script>
-
+import HeaderSide from './components/HeaderSide.vue'
+import MainSide from './components/MainSide.vue'
+import FooterSide from './components/FooterSide.vue'
 
 export default {
   name: 'App',
   components: {
+    HeaderSide,
+    
     
   }
 }
@@ -20,7 +24,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/HeaderSide.vue
@@ -0,0 +1,48 @@
+<template>
+    <header>
+        <img src="../assets/dc-logo.png" alt="dc-logo">
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderSide',
+}
+
+
+</script>
+
+<style lang="scss"scoped>
+$blue-DC:#0282f9;
+header{
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    ul{
+        list-style-type: none;
+        display: flex;
+        li{
+            padding: 5px;
+            &:hover{
+                color: $blue-DC ;
+                border-bottom: 2px solid $blue-DC;
+            }
+        }
+    }
+}
+</style>
+
